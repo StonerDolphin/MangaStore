c2d3ef41eb736445fd9051c3c0ce70dcc36509ab
camilo <cam.trivino@gmail.com>
2023-04-18 10:48:53 -0400

    avance

--- js/jquery.js
+++ js/jquery.js
@@ -1,32 +1,13 @@
-$(document).ready(function() {
-    $("#contactForm").submit(function(e) {
-      e.preventDefault(); // Prevenimos el envío del formulario para validar primero
-      var email = $("#emailInput").val();
-      var comment = $("#commentInput").val();
-      var isValid = true; // Bandera de validación
-      if (!email) {
-        $("#emailInput").addClass("is-invalid");
-        $("#emailInputError").text("Debes ingresar un correo electrónico.");
-        isValid = false;
-      } else if (!/^\S+@\S+\.\S+$/.test(email)) {
-        $("#emailInput").addClass("is-invalid");
-        $("#emailInputError").text("Debes ingresar un correo electrónico válido.");
-        isValid = false;
-      } else {
-        $("#emailInput").removeClass("is-invalid");
-        $("#emailInputError").text("");
-      }
-      if (!comment) {
-        $("#commentInput").addClass("is-invalid");
-        $("#commentInputError").text("Debes ingresar tus comentarios.");
-        isValid = false;
-      } else {
-        $("#commentInput").removeClass("is-invalid");
-        $("#commentInputError").text("");
-      }
-      if (isValid) {
-        // Si el formulario es válido, podemos enviarlo
-        this.submit();
-      }
-    });
-  });
+$(document).ready(function(){
+    $("#contactForm").submit(function (e){
+
+        e.preventDefault();
+
+        var email=$("#emailInput").value();
+        var comentario=$("commentInput").value();
+        var isValid=true;
+
+        
+        
+    })
+})
