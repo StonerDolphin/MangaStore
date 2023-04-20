d958fcefd9869cdcf9e259d25fadd0d987c21d8c
Camilo <camilo.trivino@gmail.com>
2023-04-19 20:28:56 -0400

    Validacion jquery completa

--- js/form-query.js
+++ js/form-query.js
@@ -1,24 +1,35 @@
 $(document).ready(function(){
-    $("#contactForm").submit(function (e){
-        e.preventDefault();
-        var email=$("#emailInput").value();
-        var comentario=$("commentInput").value();
-        
-        $(".error").remove();
-        if (comentario.length == 0) {
-            $('#commentInput').after('<span class="error">Este campo es obligatorio</span>');
-        }else if (comentario.length < 3) {
-            $(".error").remove();
-            $('#commentInput').after('<span class="error">Este campo debe tener mínimo 3 caracteres</span>');
-        }
-        if (email.length == 0) {
-            $('#emailInput').after('<span class="error">Este campo es obligatorio</span>');
-        } else {
-            var regEx = /^[\w-.]+@([\w-]+.)+[\w-]{2,4}$/
-            var validEmail = regEx.test(email);
-            if (!validEmail) {
-            $('#emailInput').after('<span class="error">Ingrese un Email válido</span>');
+    let email=$("#emailInput")
+    let mensaje=$("#commentInput")
+    $("#contactForm").validate({
+        rules:{
+            email:{
+                required: true,
+                email: true
+            },
+            mensaje:{
+                required: true,
+                minlength:4,
+                maxlength:1000
+            }
+        },
+        messages:{
+            correo:"Ingrese una direccion de correo valido.",
+            mensaje:{
+                required:"Se debe escribir un comentario",
+                minlength:"El comentario debe contener un minimo de 4 letras",
+                maxlength:"El comentario debe contener un maximo de 1000 letras"
             }
         }
     })
+    
+    $("#guardar").click(function(){
+        if($("#contactForm").valid()==false){
+            return;
+        }
+        
+    })
+
+    $()
 })
+
