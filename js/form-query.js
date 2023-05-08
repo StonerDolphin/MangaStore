f5d530b8e4e1653a8eb780b2de04b74ee4765f1b
Fabian Duran <f.duran@duocuc.cl>
2023-05-08 13:55:33 -0400

    validaciones para el login mas que nada colores

--- js/form-query.js
+++ js/form-query.js
@@ -35,3 +35,22 @@ $(document).ready(function(){
     $()
 })
 
+(function(){
+    $(document).ready(function(){
+            let formRegistro = document.getElementsByName('form-input');
+        for (let i = 0; i < formRegistro.length; i++) {
+            formRegistro[i].addEventListener('blur', function(){
+                if (this.value.length >= 1) {
+                    this.nextElementSibling.classList.add('active');
+                    this.nextElementSibling.classList.remove('error');
+                } else if (this.value.length = " ") {
+                    this.nextElementSibling.classList.add('error');
+                    this.nextElementSibling.classList.remove('active');
+                } else {
+                    this.nextElementSibling.classList.remove('active');
+                }
+            })
+        }
+
+    })
+}())
