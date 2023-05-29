2a0bb24452a625e515758d5405e7942dce31163c
Fabian Duran <f.duran@duocuc.cl>
2023-05-29 19:24:53 -0400

    vis

--- venta/views.py
+++ venta/views.py
@@ -3,3 +3,22 @@ from django.shortcuts import render
 def inicio(request):
     context={}
     return render (request,'venta/index.html', context)
+
+def compra(request):
+    return render (request,'venta/compra.html')
+
+def login(request):
+    return render(request,'venta/login.html')
+
+def producto(request):
+    return render(request,'venta/producto.html')
+
+def progreso(request):
+    return render(request,'venta/progreso.html')
+
+def registrarse(request):
+    return render(request,'venta/registrarse.html')
+
+def tienda(request):
+    return render(request,'venta/tienda.html')
+
