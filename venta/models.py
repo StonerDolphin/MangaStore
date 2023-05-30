d3514731dba65b8f135ac6ae961a1574d2445fe9
camilo <cam.trivino@gmail.com>
2023-05-30 10:52:53 -0400

    creacion de tablas

--- venta/models.py
+++ venta/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Manga(models.Model):
+    # Campos del modelo
+    id_manga = models.CharField(primary_key = True, max_length = 10)
+    titulo            = models.CharField(max_length  = 100)
+    autor             = models.CharField(max_length  = 100)
+    id_genero         = models.ForeignKey('genero', on_delete = models.CASCADE)
+    fecha_publicacion = models.DateField()
+    sinopsis          = models.TextField()
+
+    def __str__(self):
+        return self.titulo
+
+class genero(models.Model):
+    id_genero   = models.CharField(primary_key   =  True, max_length = 10)
+    nomb_genero = models.CharField(max_length = 100)
+
+    def __str__(self):
+        return self.nomb_genero
