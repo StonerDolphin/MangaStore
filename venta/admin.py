4db19e816abd366472b36df5ab5e90e012cfee89
Camilo <camilo.trivino@gmail.com>
2023-05-30 18:08:05 -0400

    avance model

--- venta/admin.py
+++ venta/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+admin.site.register(manga)
+admin.site.register(comuna)
+admin.site.register(cliente)
+admin.site.register(region)
+admin.site.register(editorial)
+admin.site.register(genero)
--- venta/models.py
+++ venta/models.py
@@ -1,21 +1,58 @@
 from django.db import models
+from django.contrib.auth.hashers import make_password
 
 # Create your models here.
-class Manga(models.Model):
-    # Campos del modelo
-    id_manga = models.CharField(primary_key = True, max_length = 10)
-    titulo            = models.CharField(max_length  = 100)
-    autor             = models.CharField(max_length  = 100)
-    id_genero         = models.ForeignKey('genero', on_delete = models.CASCADE)
-    fecha_publicacion = models.DateField()
-    sinopsis          = models.TextField()
-
-    def __str__(self):
-        return self.titulo
-
 class genero(models.Model):
+    # Campos del modelo
     id_genero   = models.CharField(primary_key   =  True, max_length = 10)
     nomb_genero = models.CharField(max_length = 100)
+    def __str__(self):
+        return str(self.nomb_genero)
+
+class editorial(models.Model):
+    # Campos del modelo
+    id_editorial   = models.CharField(primary_key   =  True, max_length = 10)
+    nomb_editorial = models.CharField(max_length = 100)
+    def __str__(self):
+        return str(self.nomb_editorial)
+
+class region(models.Model):
+    # Campos del modelo
+    id_region   = models.CharField(primary_key   =  True, max_length = 10)
+    nomb_region = models.CharField(max_length = 100)
+    def __str__(self):
+        return str(self.nomb_region)
+
+class comuna(models.Model):
+    # Campos del modelo
+    id_comuna   = models.CharField(primary_key   =  True, max_length = 10)
+    nomb_comuna = models.CharField(max_length = 100)
+    id_region   = models.ForeignKey('region', on_delete=models.CASCADE, db_column='id_region')
+    def __str__(self):
+        return str(self.nomb_comuna)
+
+class cliente(models.Model):
+    id_cliente = models.CharField(primary_key = True, max_length = 10)
+    usuario    = models.CharField(max_length = 100  , unique = True)
+    nombre     = models.CharField(max_length = 100)
+    email      = models.EmailField(unique    = True)
+    telefono   = models.CharField(max_length = 20)
+    id_comuna  = models.ForeignKey('comuna', on_delete=models.CASCADE, db_column='id_comuna')
 
     def __str__(self):
-        return self.nomb_genero
+        return str(self.nombre)
+class manga(models.Model):
+    # Campos del modelo
+    id_manga          = models.CharField(primary_key = True, max_length = 10)
+    titulo            = models.CharField(max_length = 100)
+    nro_volumen       = models.CharField(max_length = 10)
+    precio            = models.CharField(max_length = 100)
+    autor             = models.CharField(max_length = 100)
+    fecha_publicacion = models.DateField()
+    sinopsis          = models.TextField()
+    stock             = models.CharField(max_length = 100)
+    id_genero         = models.ForeignKey('genero', on_delete=models.CASCADE, db_column='id_genero')
+    id_editorial      = models.ForeignKey('editorial', on_delete=models.CASCADE, db_column='id_editorial')
+    def __str__(self):
+        return str(self.titulo)+" "+str(self.nro_volumen)
+
