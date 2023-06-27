92be135f3c185bd64f4a9b88cae64c1690bd639e
Camilo <camilo.trivino@gmail.com>
2023-06-26 21:05:17 -0400

    arreglos

--- mangaStore/urls.py
+++ mangaStore/urls.py
@@ -26,6 +26,4 @@ if settings.DEBUG:
     from django.conf.urls.static import static
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-if settings.DEBUG:
-    from django.conf.urls.static import static
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
--- venta/models.py
+++ venta/models.py
@@ -1,7 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
 
-
 # Create your models here.
 class Genero(models.Model):
     # Campos del modelo
@@ -9,7 +8,6 @@ class Genero(models.Model):
     nomb_genero = models.CharField(max_length = 100, blank=False, null=False)
     def __str__(self):
         return str(self.nomb_genero)
-
 
 class Editorial(models.Model):
     # Campos del modelo
@@ -29,7 +27,7 @@ class Comuna(models.Model):
     # Campos del modelo
     id_comuna   = models.AutoField(primary_key   =  True, max_length = 10)
     nomb_comuna = models.CharField(max_length = 100)
-    id_region   = models.ForeignKey('region', on_delete=models.CASCADE, db_column='id_region')
+    id_region   = models.ForeignKey(Region, on_delete=models.CASCADE, db_column='id_region')
     def __str__(self):
         return str(self.nomb_comuna)
 
@@ -39,10 +37,10 @@ class Cliente(models.Model):
     nombre    = models.CharField(max_length = 100)
     email     = models.EmailField(unique    = True)
     telefono  = models.CharField(max_length = 20)
-    id_comuna = models.ForeignKey('comuna' , on_delete = models.CASCADE, db_column   = 'id_comuna', null=True, default='null')
-
+    id_comuna = models.ForeignKey(Comuna , on_delete = models.CASCADE, db_column   = 'id_comuna', null=True, default='null')
     def __str__(self):
         return str(self.nombre)
+
 class Manga(models.Model):
     # Campos del modelo
     id_manga          = models.CharField(primary_key = True, max_length = 10)
@@ -54,35 +52,34 @@ class Manga(models.Model):
     cover             = models.ImageField(default = 'null', upload_to = 'manga')
     fecha_publicacion = models.DateField(blank=False, null=False)
     sinopsis          = models.TextField()
-    id_genero         = models.ForeignKey('genero', on_delete=models.CASCADE, db_column='id_genero')
-    id_editorial      = models.ForeignKey('editorial', on_delete=models.CASCADE, db_column='id_editorial')
+    id_genero         = models.ForeignKey(Genero, on_delete=models.CASCADE, db_column='id_genero')
+    id_editorial      = models.ForeignKey(Editorial, on_delete=models.CASCADE, db_column='id_editorial')
     def __str__(self):
         return str(self.titulo)+" "+str(self.nro_volumen)
-
-
 
 class Carrito(models.Model):
     # Campos del modelo
     id_carrito = models.CharField(primary_key=True, max_length=10)
     id_manga   = models.ForeignKey(Manga, on_delete=models.CASCADE, db_column='id_manga')
     user_id    = models.ForeignKey(Cliente, on_delete=models.CASCADE, db_column='user_id')
+
 class Orden(models.Model):
     # Campos del modelo
     nro_recibo = models.CharField(primary_key = True, max_length = 10)
     direccion  = models.CharField(max_length = 100)
     cod_postal = models.CharField(max_length = 10)
     id_comuna  = models.ForeignKey(Comuna, on_delete = models.CASCADE, db_column = 'id_comuna')
-
     def __str__(self):
         return str(self.nro_recibo)
+
 class Orden_manga(models.Model):
     # Campos del modelo
     id_orden_manga = models.CharField(primary_key = True, max_length = 10)
     id_manga       = models.ForeignKey(Manga, on_delete = models.CASCADE, db_column = 'id_manga')
     nro_recibo     = models.ForeignKey(Orden, on_delete = models.CASCADE, db_column = 'nro_recibo')
-
     def __str__(self):
         return str(self.nro_recibo)
+
 class Pago_orden(models.Model):
     # Campos del modelo
     id_pago_orden = models.CharField(primary_key = True, max_length = 10)
@@ -90,6 +87,5 @@ class Pago_orden(models.Model):
     estado_pago   = models.CharField(max_length = 100)
     user_id       = models.ForeignKey(Cliente, on_delete = models.CASCADE, db_column = 'user_id')
     nro_recibo    = models.ForeignKey(Orden, on_delete = models.CASCADE, db_column = 'nro_recibo')
-
-        return str(self.nro_recibo)
     def __str__(self):
+        return str(self.nro_recibo)
